bb6bbd2230f33258e089ef022a5eaf4fac2449db
Ivan Laver <74679323+Ereyzer@users.noreply.github.com>
2021-04-07 01:17:21 +0300

    task-08

--- js/task-05.js
+++ js/task-05.js
@@ -7,12 +7,3 @@ const {input, span} = refs;
 input.addEventListener('input', () => onSpanChangeContent(input.value));
 
 const onSpanChangeContent = (newText) => newText === '' ? span.textContent = 'незнайомець' : span.textContent = newText; 
-
-
-// {
-//     if(newText === ''){
-//         span.textContent = 'незнайомець';
-//     }else{
-//        span.textContent = newText; 
-//     }
-// }
---
+++ js/task-08.js
@@ -0,0 +1,62 @@
+const refs = {
+    controlBox: document.querySelector('#controls'),
+    input: document.querySelector('#controls input'),
+    createBtn: document.querySelector('[data-action="render"]'),
+    clearBtn: document.querySelector('[data-action="destroy"]'),
+    boxesList: document.querySelector('#boxes'), 
+}
+const {controlBox, input, createBtn, clearBtn, boxesList} = refs;
+
+const renderColor = () => Math.round(Math.random() * (250 - 0) + 0);
+const renderRGBColor = (callback) => `rgb(${callback()}, ${callback()}, ${callback()})`
+
+const createBoxes = (amount) =>{
+    const boxes = [];
+    for(let i = 0; i < amount; i += 1){
+        const box = document.createElement('div');
+        box.setAttribute('style', `background-color: ${renderRGBColor(renderColor)};
+                                    width: calc(${i} * 10px + 30px);
+                                    height: calc(${i} * 10px + 30px);`);
+        boxes.push(box);
+    }
+    boxesList.classList.toggle('container-task8')
+    createBtn.removeEventListener('click', onClickRender);
+    return boxes;
+};
+
+function addBoxes (reference, callback)  { 
+    return reference.append(...callback(Number(input.value)))
+};
+
+function onClickRender() {
+    return addBoxes(boxesList, createBoxes)
+}
+
+createBtn.addEventListener('click', onClickRender);
+
+function destroyBoxes ()  {
+    const itemsInBoxes = boxesList.querySelectorAll('div')
+    
+    itemsInBoxes.forEach(el => el.remove());
+    boxesList.classList.toggle('container-task8')
+    return createBtn.addEventListener('click', onClickRender);
+};
+
+clearBtn.addEventListener('click', destroyBoxes);
+
+
+
+
+    
+
+
+
+// Розміри найпершого div - 30px на 30px
+// Кожен наступний div після першого, повинен бути ширше і вище попереднього на   10px
+
+
+// const red = renderColor(0, 255);
+// const green = renderColor(0, 255);
+// const blue = renderColor(0, 255);
+// const color = `rgb(${red}, ${green}, ${blue})`
+// console.log(color);
